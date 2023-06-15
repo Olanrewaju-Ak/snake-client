1502b6fac0022ffa56e66eb8c6c3074499161f52
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 05:14:22 -0700

    the play.js file now holds just function connect and setUserInput calls

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-const { stderr, stdin } = require("process");
+
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-//event hadnler for standard input
-const handleUserInput = (key) => {
-  const stdout = process.stdout;
-  if (key === "\u0003") {
-    process.exit();
-  }
-  stdout.write(key);
-};
 
-//setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.group("connecting ...");
+connect();
 
 setupInput();
