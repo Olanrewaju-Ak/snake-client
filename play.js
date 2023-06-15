10abbfb119fa20aac3b6f93d0a7f97854f5961b4
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 05:07:35 -0700

    declared the handleUserInput outside of the setupInput function

--- play.js
+++ play.js
@@ -1,19 +1,22 @@
 const { stderr, stdin } = require("process");
 const { connect } = require("./client");
 
+//event hadnler for standard input
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
+  if (key === "\u0003") {
+    process.exit();
+  }
+  stdout.write(key);
+};
+
 //setup interface to handle user input from stdin
 const setupInput = function () {
   const stdin = process.stdin;
-  const stdout = process.stdout;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = (key) => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-    stdout.write(key);
-  };
+
   stdin.on("data", handleUserInput);
   return stdin;
 };
