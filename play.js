aa4c1c9356b1532c6adc3c733f1ef04dbc9af09d
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 04:51:27 -0700

    setting up standard input function

--- play.js
+++ play.js
@@ -1,2 +1,9 @@
 const { connect } = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
