ea280d8f90dddcbcf0bb81f4e81ce753c768cec1
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 05:03:21 -0700

    created a userinput handler to terminate the session when user presses ctrl + c

--- play.js
+++ play.js
@@ -1,9 +1,21 @@
+const { stderr, stdin } = require("process");
 const { connect } = require("./client");
 
+//setup interface to handle user input from stdin
 const setupInput = function () {
   const stdin = process.stdin;
+  const stdout = process.stdout;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+    stdout.write(key);
+  };
+  stdin.on("data", handleUserInput);
   return stdin;
 };
+
+setupInput();
