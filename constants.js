54c76563b276bd73cad90f1ffbee9d7794fff990
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 21:29:41 -0700

    corrected keys to strings

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
 const IP = "localhost";
 const PORT = 50541;
 
-const MOVE_UP_KEY = W;
-const MOVE_RIGHT_KEY = D;
-const MOVE_DOWN_KEY = S;
-const MOVE_LEFT_KEY = A;
+const MOVE_UP_KEY = "W";
+const MOVE_RIGHT_KEY = "D";
+const MOVE_DOWN_KEY = "S";
+const MOVE_LEFT_KEY = "A";
 
 module.exports = {
   IP,
