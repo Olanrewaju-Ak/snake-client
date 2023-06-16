ac2579044964c3af36c53c9383c32560d3906b02
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 21:31:01 -0700

    introduced constants into the input file

--- input.js
+++ input.js
@@ -1,22 +1,26 @@
+const { MOVE_UP_KEY, MOVE_RIGHT_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY } = require("./constants");
+
 // stores the active TCP object sent from the server
 let connection;
 
 //event handler for standard input
 const handleUserInput = (key) => {
+  key = key.toUpperCase();
   let message = "ouch!";
+
   if (key === "\u0003") {
     process.exit();
   }
-  if (key === "w" || key === "W") {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === "a" || key === "A") {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === "s" || key === "S") {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === "d" || key === "D") {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
 
