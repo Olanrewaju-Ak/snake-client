6e7d345515e39efc30979d9c85a1440294451a12
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-15 21:22:14 -0700

    constants to contain constant modules

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = "localhost";
+const PORT = 50541;
+
+const MOVE_UP_KEY = W;
+const MOVE_RIGHT_KEY = D;
+const MOVE_DOWN_KEY = S;
+const MOVE_LEFT_KEY = A;
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY
+};
